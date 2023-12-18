bf4b8885f8552a9dd133dc21436a6c2211b47dc4
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2023-12-18 23:06:08 +0300

    изменнеия у адрея (18.12.23)

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const App = {
     data() {
         return {
             counter: 0,
-            title: 'Список заметок'
+            title: 'Список заметок',
+            placeholder: "Введите название заметки",
+            inputValue: "",
+            notes: ['Заметка 1']
+        }
+    },
+    methods : {
+        InputChangeHandler() {
+            this.inputValue = event.target.value
         }
     }
 
