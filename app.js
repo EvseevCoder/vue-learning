fdf5c9ff9ef275a56c7a39ca94c5ad296f43c197
IGOR EVSSEV <andrey.semin.evseev@gmail.com>
2023-12-23 18:43:48 +0300

    завершение базы (23.12.23)

--- app.js
+++ app.js
@@ -5,15 +5,50 @@ const App = {
             title: 'Список заметок',
             placeholder: "Введите название заметки",
             inputValue: "",
-            notes: ['Заметка 1']
+            notes: ['Заметка 1', 'Заметка 2', 'Моя личная заметка']
         }
     },
     methods : {
-        InputChangeHandler() {
-            this.inputValue = event.target.value
+        // InputChangeHandler(event) {
+        //     this.inputValue = event.target.value
+        // },
+        addNewNote(event) {
+            if (this.inputValue.trim() != '') {
+                this.notes.push(this.inputValue.trim())
+                this.inputValue = ''
+            }
+            
+        },
+        inputKeyPress(event) {
+            if (event.key == 'Enter') {
+                this.addNewNote()
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        doubleCount() {
+            return this.notes.length * 2
+        },
+        delite(idx, event) {
+            this.notes.splice(idx, 1);
+        }
+    },
+    // Computed - оптимизация программы. Метод вызываетяс, только при изменение this.
+    computed: {
+        doubleCountComputed() {
+            console.log("computed");
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value) {
+            console.log("Изменение - ", value);
+            if (this.inputValue.length >= 30) {
+                this.inputValue = ''
+            }
         }
     }
-
 }
 
 
